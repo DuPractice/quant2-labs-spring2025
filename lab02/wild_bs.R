5a781f1f89303a94c8a08b7ad6317d1aca2b461b
Sylvan Zheng <squidgetx@gmail.com>
2025-02-06 11:31:06 -0500

    Adding comments wild_bs.R

--- lab02/wild_bs.R
+++ lab02/wild_bs.R
@@ -14,9 +14,15 @@ make_clustered_data <- function(G, N) {
 
 set.seed(123)
 df <- make_clustered_data(10, 100)
-model <- lm(y ~ treat)
+model <- lm(y ~ treat, df)
+summary(model)
 df$y_pred <- predict(model, df)
 df$residuals <- df$y - df$y_pred
 
-# Implement wild bootstrap
+# Implement wild bootstrap to estimate the SE of \beta_(treat)
+# given the observed data `df`
+# Residual sign is randomized at the cluster level (df$g)
+# Then construct y_star = y_pred + random_residual
+# Estimate y_star~treat for a bunch of different randomized residual sets
+# Retrieve sd(beta_treat) as our estimate for the true (cluster adjusted) SE 
 # ...
