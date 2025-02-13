17191bf5ef112080dd20227b56667c1a3ebcb452
Sylvan Zheng <squidgetx@gmail.com>
2025-02-13 09:52:25 -0500

    details

--- lab03/gender_wage_sim.R
+++ lab03/gender_wage_sim.R
@@ -1,14 +1,19 @@
 library(tidyverse)
-
 set.seed(1)
+
+# - Suppose the following DGP
+#      - `Occ` = -0.1 * `Fem` + $\epsilon_1$
+#      - `Wage` = -0.1 * `Fem` + 2 * `Occ` + $\epsilon_2$
+#      - `Fem` ~ Bernoulli(0.5)
+#      - $\epsilon_1, \epsilon_2, u \sim N(0,1)$
+
+# Simluate data using the above structural model 
 
 N <- 10000
 tb <- tibble(
-    # Gender is exogenous
     female = sample(c(0, 1), N, replace = T),
-    # U is exogenous
     u = rnorm(N),
-    # Fill in the rest
+
     # occ = ...
     # wage = ...
 )
