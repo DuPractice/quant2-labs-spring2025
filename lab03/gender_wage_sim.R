5ba7d7f32755b60ad5c916ce55523bf898155862
Sylvan Zheng <squidgetx@gmail.com>
2025-02-19 15:08:37 -0500

    update lab 3

--- lab03/gender_wage_sim.R
+++ lab03/gender_wage_sim.R
@@ -2,8 +2,8 @@ library(tidyverse)
 set.seed(1)
 
 # - Suppose the following DGP
-#      - `Occ` = -0.1 * `Fem` + $\epsilon_1$
-#      - `Wage` = -0.1 * `Fem` + 2 * `Occ` + $\epsilon_2$
+#      - `Occ` = -0.1 * `Fem` + u + $\epsilon_1$
+#      - `Wage` = -0.1 * `Fem` + 2 * u + `Occ` + $\epsilon_2$
 #      - `Fem` ~ Bernoulli(0.5)
 #      - $\epsilon_1, \epsilon_2, u \sim N(0,1)$
 
